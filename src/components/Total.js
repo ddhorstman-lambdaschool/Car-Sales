ebbe445794b8139b7a96dbfbeb8e75a705abb5ce
David Horstman <davidzilla27@gmail.com>
2020-04-11 17:35:20 -0400

    Fix formatting of Buy Car

--- src/components/Total.js
+++ src/components/Total.js
@@ -13,7 +13,16 @@ const Total = (props) => {
           console.log(props.car.features);
           alert(
             `Congrats on your new ${props.car.name}${
-              props.car.features.length < 1 ? "!" : " with"+props.car.features.map(feature => " "+feature.name)+"!"
+              props.car.features.length === 0
+                ? "!"
+                : props.car.features.length === 1
+                ? " with " + props.car.features[0].name + "!"
+                : " with" +
+                  props.car.features.map(
+                    (feature, i, features) =>
+                      (i + 1 === features.length ? " and " : " ") + feature.name
+                  ) +
+                  "!"
             }`
           );
           props.buyCar();
