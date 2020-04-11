d5c999e3f756ff23456a0d4c7975a8dfadbb4e09
David Horstman <davidzilla27@gmail.com>
2020-04-11 15:01:22 -0400

    Implement store, Provider, connect in App

--- src/index.js
+++ src/index.js
@@ -5,5 +5,11 @@ import App from './App';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import carFeatureReducer from "./reducers";
+
+const store = createStore(carFeatureReducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
